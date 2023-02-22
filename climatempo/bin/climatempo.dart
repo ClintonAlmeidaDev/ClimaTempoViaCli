004ad41c4372b496cc07a1b11f3978c10d56f425
ClintonAlmeidaJs <clinton.almeida10@gmail.com>
2023-02-21 21:44:47 -0300

    Final com log

--- climatempo/bin/climatempo.dart
+++ climatempo/bin/climatempo.dart
@@ -1,28 +1,23 @@
-import 'dart:ffi';
+import 'dart:async';
 import 'dart:io';
+
 import 'package:args/args.dart';
+import 'package:climatempo/alerta_server.dart';
 import 'package:climatempo/api.dart';
+import 'package:climatempo/log.dart';
 import 'package:climatempo/model/clima_tempo.dart';
 
-ArgParser criarParser() {
-  return ArgParser()
-    ..addCommand(
-        'cidade',
-        ArgParser()
-          ..addOption('nome',
-              abbr: 'n', valueHelp: 'Nome da cidade para consulta')
-          ..addOption('estado',
-              abbr: 'e', valueHelp: 'Sigla do estado para consulta'))
-    ..addCommand(
-        'agora',
-        ArgParser()
-          ..addOption('id',
-              abbr: 'i', valueHelp: 'Id da cidade para consulta do tempo'))
-    ..addFlag('help',
-        abbr: 'h', help: 'Como utilizar o programa', negatable: false);
+void main(List<String> args) {
+  runZonedGuarded(() {
+    executar(args);
+  }, (e, s) {
+    print(e);
+    print(s);
+    //salvarErro(e, s);
+  });
 }
 
-Future<void> main(List<String> args) async {
+Future<void> executar(List<String> args) async {
   final parser = criarParser();
   final argsResult = parser.parse(args);
 
@@ -32,39 +27,69 @@ Future<void> main(List<String> args) async {
   }
 
   final comando = argsResult.command;
-  try {
-    if (comando != null && comando.name == 'cidade') {
-      final nomeCidade = comando['nome'];
-      final estado = comando['estado'];
-
-      final cidades = await buscarCidades(estado: estado, nome: nomeCidade);
-      cidades.forEach((c) => print(c));
-    }
-  } catch (e) {
-    print(e);
+  if (comando != null && comando.name == 'cidade') {
+    final nomeCidade = comando['nome'];
+    final estado = comando['estado'];
+    final cidades = await buscarCidades(estado: estado, nome: nomeCidade);
+    if (comando['salvar']) await salvarCidadesIsolate(cidades);
+    cidades.forEach((c) => print(c));
   }
 
   if (comando != null && comando.name == 'agora') {
     final id = comando['id'];
     if (id == null) {
-      print("É obrigatorio informar um ['id'] de cidade");
+      print('É obrigatório informar um [-id] de cidade');
       exit(2);
     }
-    final tempo = await registrarCidadeEBuscaTempo(int.parse(id));
+    final tempo = await registrarCidadeEBuscarTempo(int.parse(id));
+    if (comando['salvar']) await salvarTempoIsolate(tempo);
     print(tempo);
+  }
+
+  if (comando != null && comando.name == 'alerta') {
+    AlertaServer().start().listen(print);
   }
 }
 
-Future<ClimaTempo> registrarCidadeEBuscaTempo(int idCidade) async {
+Future<ClimaTempo> registrarCidadeEBuscarTempo(int idCidade) async {
   await registrarCidade(idCidade: idCidade);
   return await climaAtual(idCidade: idCidade);
 }
 
 void mostrarAjuda(ArgParser parser) {
-  print('--------- HELP ----------');
+  print('--- HELP ---');
   print(parser.usage);
   for (var comando in parser.commands.entries) {
-    print('> Comando [${comando.key}');
+    print('> Comando [${comando.key}]');
     print(comando.value.usage);
   }
+}
+
+ArgParser criarParser() {
+  return ArgParser()
+    ..addCommand('alerta')
+    ..addCommand(
+      'cidade',
+      ArgParser()
+        ..addOption('nome',
+            abbr: 'n', valueHelp: 'Nome da cidade para consulta')
+        ..addOption('estado',
+            abbr: 'e', valueHelp: 'Sigla do estado para consulta')
+        ..addFlag('salvar',
+            abbr: 's',
+            help: 'Salvar o resultado em arquivo de log',
+            negatable: false),
+    )
+    ..addCommand(
+      'agora',
+      ArgParser()
+        ..addOption('id',
+            abbr: 'i', valueHelp: 'Id da cidade para consulta do tempo')
+        ..addFlag('salvar',
+            abbr: 's',
+            help: 'Salvar o resultado em arquivo de log',
+            negatable: false),
+    )
+    ..addFlag('help',
+        abbr: 'h', help: 'Como utilizar o programa', negatable: false);
 }
